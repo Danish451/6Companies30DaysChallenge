64b6085234af68a3efe747647c72ff73179239c3
Danish451 <danishkhan229@gmail.com>
2024-01-10 22:41:45 +0530

    Day 10

--- Microsoft/WiggleSort.cpp
+++ Microsoft/WiggleSort.cpp
@@ -1,11 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
+// T.c O(n*log n)
+/*
 void wiggleSort(vector<int> &nums)
 {
     int n = nums.size();
     priority_queue<int> q(nums.begin(), nums.end());
+
     for (int i = 1; i < n; i += 2)
     {
+        // top() and pop() have tc:- (log n)
         nums[i] = q.top();
         q.pop();
     }
@@ -13,6 +17,44 @@ void wiggleSort(vector<int> &nums)
     {
         nums[i] = q.top();
         q.pop();
+    }
+}*/
+// T.C: O(n)  S.C:- O(1)
+void wiggleSort(vector<int> &nums)
+{
+    if (nums.empty())
+    {
+        return;
+    }
+    int n = nums.size();
+
+    // Step 1: Find the median
+    vector<int>::iterator nth = next(nums.begin(), n / 2);
+    nth_element(nums.begin(), nth, nums.end());
+    int median = *nth;
+
+    // Step 2: Tripartie partition within O(n)-time & O(1)-space.
+    // lamda function
+    auto m = [n](int idx)       
+    { return (2 * idx + 1) % (n | 1); };
+    int first = 0, mid = 0, last = n - 1;
+    while (mid <= last)
+    {
+        if (nums[m(mid)] > median)
+        {
+            swap(nums[m(first)], nums[m(mid)]);
+            ++first;
+            ++mid;
+        }
+        else if (nums[m(mid)] < median)
+        {
+            swap(nums[m(mid)], nums[m(last)]);
+            --last;
+        }
+        else
+        {
+            ++mid;
+        }
     }
 }
 int main()
